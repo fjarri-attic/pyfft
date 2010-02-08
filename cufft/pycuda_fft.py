e1e2b484acde3cba7f0a7b660bedc17b1edd6794
Bogdan Opanchuk <bg@bk.ru>
2010-02-08 15:58:42 +1100

    added CUFFT wrapper for comparison

---
+++ cufft/pycuda_fft.py
@@ -0,0 +1,220 @@
+"""This code is written by Ying Wai (Daniel) Fan, Emory University, Dec 2009.
+Part of this modules follows the design of Derek Anderson's pycublas.py and Nicolas Pinto's cufft.py"""
+import ctypes
+from ctypes import c_uint, c_int, c_float, POINTER
+
+from pycuda_util import float2, pointer
+
+#####################
+# Wrappers to CUFFT #
+#####################
+
+import platform
+if platform.system()=='Microsoft': libcufft = ctypes.windll.LoadLibrary('cufft.dll')
+elif platform.system()=='Darwin':    libcufft = ctypes.cdll.LoadLibrary('/usr/local/cuda/lib/libcufft.dylib')
+elif platform.system()=='Linux':     libcufft = ctypes.cdll.LoadLibrary('libcufft.so')
+else:                              libcufft = ctypes.cdll.LoadLibrary('libcufft.so')
+
+cufftReal = c_float
+cufftComplex = float2
+cufftResult = c_int
+cufftHandle = c_uint
+cufftType = c_int
+
+cufftPlan1d = libcufft.cufftPlan1d
+cufftPlan1d.restype = cufftResult
+cufftPlan1d.argtypes = [POINTER(cufftHandle), c_int, cufftType, c_int]
+cufftPlan1d.__doc__ = """cufftResult cufftPlan1d(cufftHandle * plan, int nx, cufftType type, int batch)"""
+
+cufftPlan2d = libcufft.cufftPlan2d
+cufftPlan2d.restype = cufftResult
+cufftPlan2d.argtypes = [POINTER(cufftHandle), c_int, c_int, cufftType]
+cufftPlan2d.__doc__ = """cufftResult cufftPlan2d(cufftHandle * plan, int nx, int ny, cufftType type)"""
+
+cufftPlan3d = libcufft.cufftPlan3d
+cufftPlan3d.restype = cufftResult
+cufftPlan3d.argtypes = [POINTER(cufftHandle), c_int, c_int, c_int, cufftType]
+cufftPlan3d.__doc__ = """cufftResult cufftPlan3d(cufftHandle * plan, int nx, int ny, int nz, cufftType type)"""
+
+cufftDestroy = libcufft.cufftDestroy
+cufftDestroy.restype = cufftResult
+cufftDestroy.argtypes = [cufftHandle]
+cufftDestroy.__doc__ = """cufftResult cufftDestroy(cufftHandle plan)"""
+
+cufftExecC2C = libcufft.cufftExecC2C
+cufftExecC2C.restype = cufftResult
+cufftExecC2C.argtypes = [cufftHandle, POINTER(cufftComplex), POINTER(cufftComplex), c_int]
+cufftExecC2C.__doc__ = """cufftResult cufftExecC2C(cufftHandle plan, cufftComplex * idata, cufftComplex * odata, int direction)"""
+
+cufftExecR2C = libcufft.cufftExecR2C
+cufftExecR2C.restype = cufftResult
+cufftExecR2C.argtypes = [cufftHandle, POINTER(cufftReal), POINTER(cufftComplex)]
+cufftExecR2C.__doc__ = """cufftResult cufftExecR2C(cufftHandle plan, cufftReal * idata, cufftComplex * odata)"""
+
+cufftExecC2R = libcufft.cufftExecC2R
+cufftExecC2R.restype = cufftResult
+cufftExecC2R.argtypes = [cufftHandle, POINTER(cufftComplex), POINTER(cufftReal)]
+cufftExecC2R.__doc__ = """cufftResult cufftExecC2R(cufftHandle plan, cufftComplex * idata, cufftReal * odata)"""
+
+CUFFT_FORWARD = -1
+CUFFT_INVERSE = 1
+
+CUFFT_R2C = 0x2a     # Real to Complex (interleaved)
+CUFFT_C2R = 0x2c     # Complex (interleaved) to Real
+CUFFT_C2C = 0x29     # Complex to Complex, interleaved
+CUFFT_D2Z = 0x6a     # Double to Double-Complex
+CUFFT_Z2D = 0x6c     # Double-Complex to Double
+CUFFT_Z2Z = 0x69     # Double-Complex to Double-Complex
+
+####################
+# Useful utilities #
+####################
+CUFFT_SUCCESS        = 0x0
+CUFFT_INVALID_PLAN   = 0x1
+CUFFT_ALLOC_FAILED   = 0x2
+CUFFT_INVALID_TYPE   = 0x3
+CUFFT_INVALID_VALUE  = 0x4
+CUFFT_INTERNAL_ERROR = 0x5
+CUFFT_EXEC_FAILED    = 0x6
+CUFFT_SETUP_FAILED   = 0x7
+CUFFT_INVALID_SIZE   = 0x8
+CUFFT_RESULT = {
+  CUFFT_SUCCESS        : 'CUFFT_SUCCESS',
+  CUFFT_INVALID_PLAN   : 'CUFFT_INVALID_PLAN',
+  CUFFT_ALLOC_FAILED   : 'CUFFT_ALLOC_FAILED',
+  CUFFT_INVALID_TYPE   : 'CUFFT_INVALID_TYPE',
+  CUFFT_INVALID_VALUE  : 'CUFFT_INVALID_VALUE',
+  CUFFT_INTERNAL_ERROR : 'CUFFT_INTERNAL_ERROR',
+  CUFFT_EXEC_FAILED    : 'CUFFT_EXEC_FAILED',
+  CUFFT_SETUP_FAILED   : 'CUFFT_SETUP_FAILED',
+  CUFFT_INVALID_SIZE   : 'CUFFT_INVALID_SIZE'
+}
+
+def check_cufft_result(result):
+  if result != CUFFT_SUCCESS:
+    raise Exception(CUFFT_RESULT[result])
+
+class fftplan(cufftHandle):
+    def __del__(self):
+        cufftDestroy(self)
+
+##############################
+# 1D FFT Complex <-> Complex #
+##############################
+def get_1dplan(shape, batch=1):
+    plan = fftplan()
+    cufftPlan1d(plan, shape[0], CUFFT_C2C, batch)
+    return plan
+
+def _fft(direction, x, y=None, plan=None):
+    if plan is None:
+        plan = get_1dplan(x.shape)
+
+    idata = pointer(x)
+
+    if y is None:
+        odata = idata
+    else:
+        odata = pointer(y)
+
+    result = cufftExecC2C(plan, idata, odata, direction)
+    check_cufft_result(result)
+
+def gpu_fft(x, y=None, plan=None):
+    _fft(CUFFT_FORWARD, x, y, plan)
+
+def gpu_ifft(x, y=None, plan=None):
+    _fft(CUFFT_INVERSE, x, y, plan)
+
+##############################
+# 2D FFT Complex <-> Complex #
+##############################
+def get_2dplan(shape):
+    plan = fftplan()
+    cufftPlan2d(plan, shape[1], shape[0], CUFFT_C2C)
+    return plan
+
+def _fft2(direction, x, y=None, plan=None):
+    if plan is None:
+        plan = get_2dplan(x.shape)
+
+    idata = pointer(x)
+
+    if y is None:
+        odata = idata
+    else:
+        odata = pointer(y)
+
+    result = cufftExecC2C(plan, idata, odata, direction)
+    check_cufft_result(result)
+
+def gpu_fft2(x, y=None, plan=None):
+    _fft2(CUFFT_FORWARD, x, y, plan)
+
+def gpu_ifft2(x, y=None, plan=None):
+    _fft2(CUFFT_INVERSE, x, y, plan)
+
+###########################
+# 1D FFT Real <-> Complex #
+###########################
+def get_1dplan_r2c(shape,batch=1):
+    plan = fftplan()
+    cufftPlan1d(plan, shape[0], CUFFT_R2C, batch)
+    return plan
+
+def get_1dplan_c2r(shape,batch=1):
+    plan = fftplan()
+    cufftPlan1d(plan, shape[0], CUFFT_C2R, batch)
+    return plan
+
+def gpu_rfft(x, y, plan=None):
+    idata = pointer(x)
+    odata = pointer(y)
+    if plan is None:
+        plan = get_1dplan_r2c((x.size,))
+    result = cufftExecR2C(plan, idata, odata)
+    check_cufft_result(result)
+
+def gpu_irfft(x, y, plan=None):
+    idata = pointer(x)
+    odata = pointer(y)
+    if plan is None:
+        plan = get_1dplan_c2r((y.size,))
+    result = cufftExecC2R(plan, idata, odata)
+    check_cufft_result(result)
+
+###########################
+# 2D FFT Real <-> Complex #
+###########################
+def get_2dplan_r2c(shape):
+    plan = fftplan()
+    cufftPlan2d(plan, shape[0], shape[1], CUFFT_R2C)
+    return plan
+
+def get_2dplan_c2r(shape):
+    plan = fftplan()
+    cufftPlan2d(plan, shape[0], shape[1], CUFFT_C2R)
+    return plan
+
+def gpu_rfft2(x, y, plan=None):
+    idata = pointer(x)
+    odata = pointer(y)
+    if plan is None:
+        plan = get_2dplan_r2c(x.shape)
+    result = cufftExecR2C(plan, idata, odata)
+    check_cufft_result(result)
+
+def gpu_irfft2(x, y, plan=None):
+    idata = pointer(x)
+    odata = pointer(y)
+    if plan is None:
+        plan = get_2dplan_c2r(y.shape)
+    result = cufftExecC2R(plan, idata, odata)
+    check_cufft_result(result)
+
+def malloc_for_r2c(idata):
+    from pycuda.gpuarray import GPUArray
+    input_shape = idata.shape
+    output_shape = (input_shape[0], input_shape[1]/2 + 1)
+    temp = GPUArray(output_shape, 'complex64')
+    return temp
--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 from pycuda.autoinit import device
 import pycuda.driver as cuda
+import pycuda.gpuarray as gpuarray
 from pycudafft import *
 import numpy
 
+from cufft.pycuda_fft import *
+
+
+import time
 import math
 
 def rand_complex(*dims):
-	#real = numpy.random.randn(*dims)
-	#imag = numpy.random.randn(*dims)
-	#return (real + 1j * imag).astype(numpy.complex64)
-
-	return (numpy.ones(dims) + 1j * numpy.ones(dims)).astype(numpy.complex64)
+	real = numpy.random.randn(*dims)
+	imag = numpy.random.randn(*dims)
+	return (real + 1j * imag).astype(numpy.complex64)
+	#return (numpy.ones(dims) + 1j * numpy.ones(dims)).astype(numpy.complex64)
 
 def difference(arr1, arr2):
-	return math.sqrt(numpy.cov(arr1.ravel() - arr2.ravel(), bias=1))
+	return math.sqrt(numpy.cov(arr1.ravel() - arr2.ravel(), bias=1)) / arr1.size
 
 def numpy_fft_base(data, dim, len, batch, func):
 	res = []
@@ -30,6 +34,11 @@ def numpy_fft_base(data, dim, len, batch, func):
 
 	return numpy.concatenate(tuple(res), axis=dim)
 
+def timefunc(func, *args, **kwds):
+	t1 = time.time()
+	func(*args, **kwds)
+	return (time.time() - t1) * 1000
+
 def test(dim, x, y, z, batch):
 
 	if x * y * z * batch * 8 > 2 ** 26:
@@ -38,75 +47,72 @@ def test(dim, x, y, z, batch):
 
 	if dim == clFFT_1D:
 		data = rand_complex(x * batch)
-		batch_len = x
-		fft = numpy.fft.fft
-		ifft = numpy.fft.ifft
+		cufft_plan = get_1dplan((x,), batch)
 	elif dim == clFFT_2D:
 		data = rand_complex(x, y * batch)
-		batch_len = y
-		fft = numpy.fft.fft2
-		ifft = numpy.fft.ifft2
 	elif dim == clFFT_3D:
 		data = rand_complex(x, y, z * batch)
-		batch_len = z
-		#fft = numpy.fft.fft3
-		#ifft = numpy.fft.ifft3
 
-	# TODO: Will not work under 32-bit python with 2D and 3D
-	#numpy_transformed = numpy_fft_base(data, dim, batch_len, batch, fft)
-	#numpy_back = numpy_fft_base(numpy_transformed, dim, batch_len, batch, ifft)
-	#numpy_err = difference(numpy_back, data)
-	#if numpy_err > 1e-6:
-	#	raise Exception("numpy error: " + str(numpy_err))
+	# Prepare arrays
+	res = numpy.empty(data.shape, dtype=numpy.complex64)
+	cufft_res = numpy.empty(data.shape, dtype=numpy.complex64)
+	pyfft_res = numpy.empty(data.shape, dtype=numpy.complex64)
 
-	res = numpy.empty(data.shape).astype(numpy.complex64)
+	a_gpu = gpuarray.to_gpu(data)
+	b_gpu = gpuarray.GPUArray(data.shape, dtype=data.dtype)
 
+	# Prepare pycudaftt plan
 	plan = FFTPlan(x, y, z, dim)
-	a_gpu = cuda.mem_alloc(data.nbytes)
-	b_gpu = cuda.mem_alloc(data.nbytes)
-	cuda.memcpy_htod(a_gpu, data)
-	clFFT_ExecuteInterleaved(plan, batch, clFFT_Forward, a_gpu, b_gpu)
 
-	#cuda.memcpy_dtoh(res, b_gpu)
-	#print res
+	# CUFFT forward transform
+	cufft_time = timefunc(gpu_fft, a_gpu, b_gpu, plan=cufft_plan)
+	print "CUFFT Forward total call time: " + str(cufft_time) + " ms"
+	cufft_res = b_gpu.get()
 
-	clFFT_ExecuteInterleaved(plan, batch, clFFT_Forward, b_gpu, a_gpu)
-	cuda.memcpy_dtoh(res, a_gpu)
+	# pycudafft forward transform
+	a_gpu.set(data)
+	pyfft_time_fw = timefunc(clFFT_ExecuteInterleaved, plan, batch, clFFT_Forward, a_gpu.gpudata, b_gpu.gpudata)
+	print "PyCuda FFT Forward total call time: " + str(pyfft_time_fw) + " ms"
+	pyfft_res = b_gpu.get()
 
+	# compare CUFFT and pycudafft results
+	diff_err = difference(cufft_res, pyfft_res)
+	if diff_err > 1e-6:
+		raise Exception("Difference between pycudafft and cufft: " + str(diff_err))
+
+	# pycudafft inerse transform
+	clFFT_ExecuteInterleaved(plan, batch, clFFT_Inverse, b_gpu.gpudata, a_gpu.gpudata)
+	res = a_gpu.get()
 	res = res / (x * y * z)
 
-	#print data
-	#print res
-
-	cuda_err = difference(res, data)
-	if cuda_err > 1e-6:
-		raise Exception("cuda error: " + str(cuda_err))
+	# compare forward-inverse result with initial data
+	pycudafft_err = difference(res, data)
+	if pycudafft_err > 1e-6:
+		raise Exception("pycudafft forward-inverse error: " + str(pycudafft_err))
 
 def runTest(dim, x, y, z, batch):
-#	try:
-		print "--- Dim: " + str(dim + 1) + ", " + str([x, y, z]) + ", batch " + str(batch) + ":"
+	#try:
+		print "--- Dim: " + str(dim + 1) + ", " + str([x, y, z]) + ", batch " + str(batch)
 		test(dim, x, y, z, batch)
-#	except Exception, e:
-#		print "--- Dim: " + str(dim + 1) + ", " + str([x, y, z]) + ", batch " + str(batch) + ":"
-#		print str(e)
-
+	#except Exception, e:
+	#	print "Failed: " + str(e)
 
 def runTests():
-	for batch in [1, 3, 4, 10]:
+	for batch in [10, 100, 1000, 5000]:
 
 		# 1D
 		for x in [8, 10]:
 			runTest(clFFT_1D, 2 ** x, 1, 1, batch)
 
 		# 2D
-		for x in [1, 3, 4, 7, 8, 10]:
-			for y in [1, 3, 4, 7, 8, 10]:
-				runTest(clFFT_2D, 2 ** x, 2 ** y, 1, batch)
+		#for x in [1, 3, 4, 7, 8, 10]:
+		#	for y in [1, 3, 4, 7, 8, 10]:
+		#		runTest(clFFT_2D, 2 ** x, 2 ** y, 1, batch)
 
 		# 3D
-		for x in [1, 3, 7, 10]:
-			for y in [1, 3, 7, 10]:
-				for z in [1, 3, 7, 10]:
-					runTest(clFFT_3D, 2 ** x, 2 ** y, 2 ** z, batch)
+		#for x in [1, 3, 7, 10]:
+		#	for y in [1, 3, 7, 10]:
+		#		for z in [1, 3, 7, 10]:
+		#			runTest(clFFT_3D, 2 ** x, 2 ** y, 2 ** z, batch)
 
 runTests()
