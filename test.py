04bdd827e9c38cfd80dec55943451ba4f97d1d89
Bogdan Opanchuk <bg@bk.ru>
2010-02-09 17:48:31 +1100

    test: added several iterations, synchronization and GFLOPS calculation for perf tests

--- test.py
+++ test.py
@@ -81,6 +81,7 @@ def testPerformance(x, y, z):
 
 	buf_size_bytes = MAX_BUFFER_SIZE * 1024 * 1024
 	value_size = 8
+	iterations = 10
 
 	batch = buf_size_bytes / (x * y * z * value_size)
 
@@ -96,12 +97,20 @@ def testPerformance(x, y, z):
 
 	plan = FFTPlan(x, y, z, dim)
 
-	start_time = time.time()
-	for i in xrange(10):
-		clFFT_ExecuteInterleaved(plan, batch, clFFT_Forward, a_gpu.gpudata, b_gpu.gpudata)
-	t = (time.time() - start_time) * 100
+	start = cuda.Event()
+	stop = cuda.Event()
 
-	print "* pycufft performance: " + str([x, y, z]) + ", batch " + str(batch) + ": " + str(t) + " ms"
+	start.record()
+	for i in xrange(iterations):
+		clFFT_ExecuteInterleaved(plan, batch, clFFT_Forward, a_gpu.gpudata, b_gpu.gpudata)
+	stop.record()
+	stop.synchronize()
+	t = stop.time_since(start) / 1000.0 # in seconds
+
+	gflop = 5.0e-9 * (log2(x) + log2(y) + log2(z)) * x * y * z * batch
+
+	print "* pycudafft performance: " + str([x, y, z]) + ", batch " + str(batch) + ": " + \
+		str(t * 1000) + " ms, " + str(gflop / t) + " GFLOPS"
 
 def testErrors(x, y, z, batch):
 
@@ -131,7 +140,7 @@ def testErrors(x, y, z, batch):
 	cufft_plan.execute(b_gpu, a_gpu, CUFFT_INVERSE)
 	cufft_res = a_gpu.get() / (x * y * z)
 
-	cufft_err = difference(cufft_res, data)
+	cufft_err = difference(cufft_res, data, batch)
 
 	del cufft_plan # forcefully release GPU memory
 
@@ -145,7 +154,7 @@ def testErrors(x, y, z, batch):
 	clFFT_ExecuteInterleaved(plan, batch, clFFT_Inverse, b_gpu.gpudata, a_gpu.gpudata)
 	pyfft_res_outplace = a_gpu.get() / (x * y * z)
 
-	pycudafft_err_outplace = difference(pyfft_res_outplace, data)
+	pycudafft_err_outplace = difference(pyfft_res_outplace, data, batch)
 
 	a_gpu.set(data)
 	clFFT_ExecuteInterleaved(plan, batch, clFFT_Forward, a_gpu.gpudata, a_gpu.gpudata)
@@ -154,11 +163,11 @@ def testErrors(x, y, z, batch):
 	clFFT_ExecuteInterleaved(plan, batch, clFFT_Inverse, a_gpu.gpudata, a_gpu.gpudata)
 	pyfft_res_inplace = a_gpu.get() / (x * y * z)
 
-	pycudafft_err_inplace = difference(pyfft_res_inplace, data)
+	pycudafft_err_inplace = difference(pyfft_res_inplace, data, batch)
 
 	# check cases where there shouldn't be any errors at all
-	pycudafft_err_inout_fw = difference(pyfft_fw_inplace, pyfft_fw_outplace)
-	pycudafft_err_inout_res = difference(pyfft_res_inplace, pyfft_res_outplace)
+	pycudafft_err_inout_fw = difference(pyfft_fw_inplace, pyfft_fw_outplace, batch)
+	pycudafft_err_inout_res = difference(pyfft_res_inplace, pyfft_res_outplace, batch)
 	assert pycudafft_err_inout_fw < small_epsilon, "inplace-outplace intermediate error: " + str(pycudafft_err_inout_fw)
 	assert pycudafft_err_inout_res < small_epsilon, "inplace-outplace final error: " + str(pycudafft_err_inout_res)
 
@@ -172,7 +181,7 @@ def testErrors(x, y, z, batch):
 	if pycudafft_err_outplace > large_epsilon:
 		raise Exception("pycudafft forward-inverse outplace error: " + str(pycudafft_err_outplace))
 
-	diff_err = difference(cufft_fw, pyfft_fw_inplace)
+	diff_err = difference(cufft_fw, pyfft_fw_inplace, batch)
 	if diff_err > large_epsilon:
 		raise Exception("Difference between pycudafft and cufft: " + str(diff_err))
 
@@ -210,5 +219,5 @@ def runPerformanceTests():
 	testPerformance(32, 32, 128)
 	testPerformance(128, 128, 128)
 
-runErrorTests()
-#runPerformanceTests()
+#runErrorTests()
+runPerformanceTests()
