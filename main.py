7a87473e3f4255629b52eb6ffdac993b37cea07b
Bogdan Opanchuk <bg@bk.ru>
2010-02-09 13:17:00 +1100

    main: separated error and performance tests

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ import numpy
 
 from cufft.pycuda_fft import *
 
-
 import time
 import math
+
+MAX_BUFFER_SIZE = 16 # in megabytes
 
 def rand_complex(*dims):
 	real = numpy.random.randn(*dims)
@@ -35,91 +36,142 @@ def numpy_fft_base(data, dim, len, batch, func):
 
 	return numpy.concatenate(tuple(res), axis=dim)
 
-def timefunc(func, *args, **kwds):
-	t1 = time.time()
-	func(*args, **kwds)
-	return (time.time() - t1) * 1000
+def getDim(x, y, z):
+	if z == 1:
+		if y == 1:
+			return clFFT_1D
+		else:
+			return clFFT_2D
+	else:
+		return clFFT_3D
 
-def test(dim, x, y, z, batch):
+def getTestData(dim, x, y, z, batch):
+	if dim == clFFT_1D:
+		return rand_complex(x * batch)
+	elif dim == clFFT_2D:
+		return rand_complex(x, y * batch)
+	elif dim == clFFT_3D:
+		return rand_complex(x, y, z * batch)
 
-	if x * y * z * batch * 8 > 2 ** 26:
-		print "Array size is " + str(x * y * z * batch * 8 / 1024 / 1024) + " Mb - test skipped"
+def testPerformance(x, y, z):
+
+	buf_size_bytes = MAX_BUFFER_SIZE * 1024 * 1024
+	value_size = 8
+
+	batch = buf_size_bytes / (x * y * z * value_size)
+
+	if batch == 0:
+		print "Buffer size is too big, skipping test"
 		return
+
+	dim = getDim(x, y, z)
+	data = getTestData(dim, x, y, z, batch)
+
+	a_gpu = gpuarray.to_gpu(data)
+	b_gpu = gpuarray.GPUArray(data.shape, dtype=data.dtype)
+
+	plan = FFTPlan(x, y, z, dim)
+
+	start_time = time.time()
+	for i in xrange(10):
+		clFFT_ExecuteInterleaved(plan, batch, clFFT_Forward, a_gpu.gpudata, b_gpu.gpudata)
+	t = (time.time() - start_time) * 100
+
+	print "* pycufft performance: " + str([x, y, z]) + ", batch " + str(batch) + ": " + str(t) + " ms"
+
+def testErrors(x, y, z, batch):
+
+	buf_size_bytes = MAX_BUFFER_SIZE * 1024 * 1024
+	value_size = 8
 	epsilon = 1e-2
 
-	if dim == clFFT_1D:
-		data = rand_complex(x * batch)
-	elif dim == clFFT_2D:
-		data = rand_complex(x, y * batch)
-	elif dim == clFFT_3D:
-		data = rand_complex(x, y, z * batch)
+	if x * y * z * batch * value_size > buf_size_bytes:
+		print "Array size is " + str(x * y * z * batch * value_size / 1024 / 1024) + " Mb - test skipped"
+		return
+
+	dim = getDim(x, y, z)
+	data = getTestData(dim, x, y, z, batch)
 
 	# Prepare arrays
 	a_gpu = gpuarray.to_gpu(data)
 	b_gpu = gpuarray.GPUArray(data.shape, dtype=data.dtype)
 
-	# Prepare plans
-	plan = FFTPlan(x, y, z, dim)
+	# CUFFT tests
 	cufft_plan = CUFFTPlan(x, y, z, batch)
 
-	# CUFFT forward transform
-	cufft_time = timefunc(cufft_plan.execute, a_gpu, b_gpu, -1)
-	print "CUFFT Forward total call time: " + str(cufft_time) + " ms"
+	cufft_plan.execute(a_gpu, b_gpu, CUFFT_FORWARD)
 	cufft_fw = b_gpu.get()
 
-	# CUFFT inverse transgorm
-	cufft_time = timefunc(cufft_plan.execute, b_gpu, a_gpu, 1)
+	cufft_plan.execute(b_gpu, a_gpu, CUFFT_INVERSE)
 	cufft_res = a_gpu.get() / (x * y * z)
 
-	# pycudafft forward transform
+	del cufft_plan # forcefully release GPU memory
+
 	a_gpu.set(data)
-	pyfft_time_fw = timefunc(clFFT_ExecuteInterleaved, plan, batch, clFFT_Forward, a_gpu.gpudata, b_gpu.gpudata)
-	print "PyCuda FFT Forward total call time: " + str(pyfft_time_fw) + " ms"
+
+	# pycudafft tests
+	plan = FFTPlan(x, y, z, dim)
+
+	clFFT_ExecuteInterleaved(plan, batch, clFFT_Forward, a_gpu.gpudata, b_gpu.gpudata)
 	pyfft_fw = b_gpu.get()
 
-	# pycudafft inverse transform
 	clFFT_ExecuteInterleaved(plan, batch, clFFT_Inverse, b_gpu.gpudata, a_gpu.gpudata)
 	pyfft_res = a_gpu.get() / (x * y * z)
 
 	# compare CUFFT and pycudafft results
 	cufft_err = difference(cufft_res, data)
-	print "cufft forward-inverse error: " + str(cufft_err)
 	if cufft_err > epsilon:
 		raise Exception("cufft forward-inverse error: " + str(cufft_err))
 
 	pycudafft_err = difference(pyfft_res, data)
-	print "pycudafft forward-inverse error: " + str(pycudafft_err)
 	if pycudafft_err > epsilon:
 		raise Exception("pycudafft forward-inverse error: " + str(pycudafft_err))
 
 	diff_err = difference(cufft_fw, pyfft_fw)
-	print "pycudafft - cufft error: " + str(diff_err)
 	if diff_err > epsilon:
 		raise Exception("Difference between pycudafft and cufft: " + str(diff_err))
 
-def runTest(dim, x, y, z, batch):
+	print "* error tests for " + str([x, y, z]) + ", batch " + str(batch) + \
+		": pycudafft=" + str(pycudafft_err) + \
+		", cufft=" + str(cufft_err) + \
+		", reference_check=" + str(diff_err)
+
+def runTest(dim, x, y, z, batch=None):
 	#try:
-		print "--- Dim: " + str(dim + 1) + ", " + str([x, y, z]) + ", batch " + str(batch)
+
 		test(dim, x, y, z, batch)
 	#except Exception, e:
 	#	print "Failed: " + str(e)
 
-def runTests():
-	for batch in [16, 128, 1024, 4096]:
+def runErrorTests():
+	for batch in [1, 16, 128, 1024, 4096]:
 
 		# 1D
 		for x in [8, 10]:
-			runTest(clFFT_1D, 2 ** x, 1, 1, batch)
+			testErrors(2 ** x, 1, 1, batch)
 
 		# 2D
 		for x in [4, 7, 8, 10]:
 			for y in [4, 7, 8, 10]:
-				runTest(clFFT_2D, 2 ** x, 2 ** y, 1, batch)
+				testErrors(2 ** x, 2 ** y, 1, batch)
 
 		# 3D
-		#for x in [1, 3, 7, 10]:
-		#	for y in [1, 3, 7, 10]:
-		#		for z in [1, 3, 7, 10]:
-		#			runTest(clFFT_3D, 2 ** x, 2 ** y, 2 ** z, batch)
+		for x in [4, 7, 10]:
+			for y in [4, 7, 10]:
+				for z in [4, 7, 10]:
+					testErrors(2 ** x, 2 ** y, 2 ** z, batch)
 
-runTests()
+def runPerformanceTests():
+	testPerformance(16, 1, 1)
+	testPerformance(1024, 1, 1)
+	testPerformance(8192, 1, 1)
+	testPerformance(16, 16, 1)
+	testPerformance(128, 128, 1)
+	testPerformance(1024, 1024, 1)
+	testPerformance(16, 16, 16)
+	testPerformance(32, 32, 128)
+	testPerformance(128, 128, 128)
+
+runErrorTests()
+#runPerformanceTests()
+
