a97056de4e7a95add85cd98edea58c652dcb0f4d
Bogdan Opanchuk <mantihor@gmail.com>
2010-03-20 19:13:20 +1100

    test/test_doc: updated according to current interface

--- test/test_doc.py
+++ test/test_doc.py
@@ -8,34 +8,33 @@ FFT for PyCuda and PyOpenCL
 Introduction
 ------------
 
-At the moment of writing this, PyCuda experienced problems with CUFFT caused by nVidia's
-architectural decisions. As a result, this piece of software was written. It contains
-implementation of batched FFT, ported from `Apple's OpenCL implementation
+This module contains implementation of batched FFT, ported from `Apple's OpenCL implementation
 <https://developer.apple.com/mac/library/samplecode/OpenCL_FFT/index.html>`_.
-OpenCL's ideology of constructing kernel code on the fly maps perfectly on PyCuda,
+OpenCL's ideology of constructing kernel code on the fly maps perfectly on
+`PyCuda <http://mathema.tician.de/software/pycuda>`_/`PyOpenCL <http://mathema.tician.de/software/pyopencl>`_,
 and variety of Python's templating engines makes code generation simpler. I used
 `mako <http://pypi.python.org/pypi/Mako>`_ templating engine, simply because of
 the personal preference. The code can be easily changed to use any other engine.
 
-In addition, project repository contains wrapper, which adds batch support for CUFFT and
-its mapping to PyCuda. They were added for testing purposes and will not be actively
-supported.
-
 Quick Start
 -----------
 
-The usage is quite simple. First, import ``pycudafft`` and ``numpy``:
+The usage is quite simple. First, import ``numpy`` and plan creation interface from ``pyfft``
+(let us use cuda in this example):
 
- >>> import pycuda.autoinit
- >>> import pycuda.gpuarray as gpuarray
- >>> from pycudafft import FFTPlan
+ >>> from pyfft.cuda import Plan
  >>> import numpy
 
 Then the plan must be created. The creation is not very fast, mainly because of the
-compilation speed. But, fortunately, ``PyCuda`` caches compiled sources, so if you
+compilation speed. But, fortunately, ``PyCuda`` and ``PyOpenCL`` cache compiled sources, so if you
 use the same plan for each run of your program, it will be created pretty fast.
 
- >>> plan = FFTPlan((16, 16))
+Since we are using Cuda, it must be initialized before any Cuda functions are called.
+In addition, we will need ``gpuarray`` module to pass data to and from GPU:
+
+ >>> import pycuda.autoinit
+ >>> import pycuda.gpuarray as gpuarray
+ >>> plan = Plan((16, 16))
 
 Now, let's prepare simple test array and try to execute plan over it:
 
